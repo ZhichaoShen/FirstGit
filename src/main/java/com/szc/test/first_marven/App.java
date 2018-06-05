0ee4e7745efaaa32718a603484410476633a5156
zhishen <609401003@qq.com>
2018-06-05 22:15:32 +0800

    First Commit

---
+++ src/main/java/com/szc/test/first_marven/App.java
@@ -0,0 +1,13 @@
+package com.szc.test.first_marven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
